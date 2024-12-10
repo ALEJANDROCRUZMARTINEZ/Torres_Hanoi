86612e1315febf1f3a3f72ab252d7d15dea609d5
Alejandro Cruz Martinez <230110322@itsoeh.edu.mx>
2024-12-10 07:47:23 -0600

    Se crearon las pilas
    Signed-off-by: Alejandro Cruz Martinez <230110322@itsoeh.edu.mx>

--- cursodsa-master/learning/Stacks/HanoiGame.py
+++ cursodsa-master/learning/Stacks/HanoiGame.py
@@ -3,7 +3,14 @@ from StackHanoi import Stack
 print("\n¡Vamos a jugar a las torres de Hanoi!")
 
 #Crear las pilas
+stacks = []
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
 
+stacks.append(left_stack)
+stacks.append(middle_stack)
+stacks.append(right_stack)
 
 
 #Configurar el juego
