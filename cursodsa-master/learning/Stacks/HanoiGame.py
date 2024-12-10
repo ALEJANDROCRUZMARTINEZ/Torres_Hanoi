988c5d6dc54db7434f1aaa4e4b6241c5c90a276e
Alejandro Cruz Martinez <230110322@itsoeh.edu.mx>
2024-12-10 07:48:55 -0600

    Se configuro el juego
    Signed-off-by: Alejandro Cruz Martinez <230110322@itsoeh.edu.mx>

--- cursodsa-master/learning/Stacks/HanoiGame.py
+++ cursodsa-master/learning/Stacks/HanoiGame.py
@@ -14,7 +14,16 @@ stacks.append(right_stack)
 
 
 #Configurar el juego
+num_disks = int(input("¿Con cuántos discos quieres jugar? "))
 
+while num_disks < 3:
+    num_disks = int(input("Ingresa un número mayor o igual a 3\n"))
+
+for i in range(num_disks, 0, -1):
+    left_stack.push(i)  # Cambiado a push
+
+
+num_optimal_moves = (2 ** num_disks) - 1
 
 
 #Obtener entrada del usuario
